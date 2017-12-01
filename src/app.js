d1169d70ce8356fd0ad4337c90d9aa03eae8d3c6
sabrichu <sabrichu@gmail.com>
2017-11-30 22:25:29 -0800

    Rough pattern scheduler

--- src/app.js
+++ src/app.js
@@ -6,20 +6,80 @@ const patterns = require('./patterns');
 //         sliceLimit: 1
 //     }
 // });
+function partial(func) {
+    let args = Array.prototype.slice.call(arguments, 1);
 
-let rrentPattern = patterns.randomVisibilityAndColor({
+    return function() {
+        let allArguments = args.concat(Array.prototype.slice.call(arguments));
+        return func.apply(this, allArguments);
+    };
+}
+let currentPattern = null;
+let pattern1 = partial(patterns.randomVisibilityAndColor, {
     speed: 100
 });
+let pattern2 = partial(patterns.randomOpacity, {
+    speed: 100,
+    surfaceOptions: {
+        levelLimit: 3
+    }
+});
+let pattern3 = partial(patterns.randomOpacity, {
+    speed: 100,
+    surfaceOptions: {
+        levelLimit: 2
+    }
+});
+let pattern4 = partial(patterns.randomOpacity, {
+    speed: 100,
+    surfaceOptions: {
+        levelLimit: 1
+    }
+});
 
-// let anotherPattern = patterns.randomOpacity({
-//     speed: 100,
-//     surfaceOptions: {
-//         levelLimit: 1
-//     }
-// });
+const setNewPattern = (pattern, time, nextPattern) => {
+    setTimeout(() => {
+        patterns.clearPattern(currentPattern);
+        currentPattern = pattern();
 
-setTimeout(() => {
-    // patterns.clearPattern(currentPattern);
+        if (typeof nextPattern === 'function') {
+            nextPattern();
+        }
+    }, time);
+};
 
-    // currentPattern = patterns.randomToggle(1000);
-}, 5000);
+currentPattern = pattern1();
+
+setNewPattern(
+    pattern2,
+    1000,
+    () => {
+        setNewPattern(
+            pattern3,
+            1000,
+            () => {
+                setNewPattern(
+                    pattern4,
+                    1000
+                );
+            }
+        )
+    }
+);
+
+// setTimeout(() => {
+//     patterns.clearPattern(currentPattern);
+
+//     currentPattern = patterns.randomOpacity({
+//         speed: 100,
+//         surfaceOptions: {
+//             levelLimit: 1
+//         }
+//     });
+
+
+//     setTimeout(() => {
+//         patterns.clearPattern(currentPattern);
+
+//     }, 5000);
+// }, 5000);
