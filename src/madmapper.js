e06bf0a960b457db93862fa717df07e82f1864c6
sabrichu <sabrichu@gmail.com>
2017-12-02 20:59:23 -0800

    Storm app - finito! Hurray

--- src/madmapper.js
+++ src/madmapper.js
@@ -1,30 +1,36 @@
 const osc = require('node-osc');
 const oscClient = new osc.Client('127.0.0.1', 8917);
+const oscServer = new osc.Server(8918, '127.0.0.1');
+
+// oscServer.on('message', function(msg, rinfo) {
+//     console.log("TUIO message:");
+//     console.log(msg);
+// });
 
 const mathUtils = require('./utils');
 
-const fadeIn = (surfaceNames, speedDenominator = 1000) => {
+const fadeIn = (surfaceNames, maxOpacity = 1) => {
     let start = 0;
     let fade = setInterval(() => {
-        if (start < 1) {
+        if (start < maxOpacity) {
             surfaceNames.forEach((surfaceName) => {
                 oscClient.send(`/surfaces/${surfaceName}/opacity`, start);
             });
-            start += 1 / speedDenominator;
+            start += maxOpacity / 1000;
         } else {
             clearInterval(fade);
         }
     }, 0);
 };
 
-const fadeOut = (surfaceNames, speedDenominator = 1000) => {
-    let start = 1;
+const fadeOut = (surfaceNames, maxOpacity = 1) => {
+    let start = maxOpacity;
     let fade = setInterval(() => {
         if (start >= 0) {
             surfaceNames.forEach((surfaceName) => {
                 oscClient.send(`/surfaces/${surfaceName}/opacity`, start);
             });
-            start -= 1 / speedDenominator;
+            start -= maxOpacity / 1000;
         } else {
             clearInterval(fade);
         }
@@ -67,19 +73,48 @@ const hide = (surfaceNames) => {
     });
 };
 
-const resetColorAll = (surfaceNames) => {
+const resetColor = (surfaceNames) => {
     surfaceNames.forEach((surfaceName) => {
+        // oscClient.send(`/getValues?url=/surfaces/${surfaceName}/red&normalized=1`);
         oscClient.send(`/surfaces/${surfaceName}/red`, 1);
         oscClient.send(`/surfaces/${surfaceName}/green`, 1);
         oscClient.send(`/surfaces/${surfaceName}/blue`, 1);
     });
 };
 
-const setRandomColor = (surfaceNames) => {
+const setRed = (surfaceNames) => {
     surfaceNames.forEach((surfaceName) => {
-        oscClient.send(`/surfaces/${surfaceName}/red`, mathUtils.getRandomFloat());
-        oscClient.send(`/surfaces/${surfaceName}/green`, mathUtils.getRandomFloat());
-        oscClient.send(`/surfaces/${surfaceName}/blue`, mathUtils.getRandomFloat());
+        oscClient.send(`/surfaces/${surfaceName}/red`, 1);
+        oscClient.send(`/surfaces/${surfaceName}/green`, 0);
+        oscClient.send(`/surfaces/${surfaceName}/blue`, 0);
+    });
+};
+
+const setBlue = (surfaceNames) => {
+    surfaceNames.forEach((surfaceName) => {
+        oscClient.send(`/surfaces/${surfaceName}/red`, 0);
+        oscClient.send(`/surfaces/${surfaceName}/green`, 0);
+        oscClient.send(`/surfaces/${surfaceName}/blue`, 1);
+    });
+};
+
+const setGreen = (surfaceNames) => {
+    surfaceNames.forEach((surfaceName) => {
+        oscClient.send(`/surfaces/${surfaceName}/red`, 0);
+        oscClient.send(`/surfaces/${surfaceName}/green`, 1);
+        oscClient.send(`/surfaces/${surfaceName}/blue`, 0);
+    });
+};
+
+const setRandomColor = (surfaceNames) => {
+    let red = mathUtils.getRandomFloat();
+    let green = mathUtils.getRandomFloat();
+    let blue = mathUtils.getRandomFloat();
+
+    surfaceNames.forEach((surfaceName) => {
+        oscClient.send(`/surfaces/${surfaceName}/red`, red);
+        oscClient.send(`/surfaces/${surfaceName}/green`, green);
+        oscClient.send(`/surfaces/${surfaceName}/blue`, blue);
     });
 };
 
@@ -91,8 +126,8 @@ const setRandomVisibility = (surfaceNames) => {
     });
 };
 
-const setRandomOpacity = (surfaceNames) => {
-    let random = Math.random();
+const setRandomOpacity = (surfaceNames, minimum = 0) => {
+    let random = mathUtils.getRandomFloat(minimum);
 
     surfaceNames.forEach((surfaceName) => {
         oscClient.send(`/surfaces/${surfaceName}/opacity`, random);
@@ -123,18 +158,34 @@ const setRandomBlue = (surfaceNames) => {
     });
 };
 
+const playForward = (mediaName) => {
+    oscClient.send(`/medias/${mediaName}/play_forward`, 1);
+    oscClient.send(`/medias/${mediaName}/play_backward`, 0);
+};
+
+const playBackward = (mediaName) => {
+    oscClient.send(`/medias/${mediaName}/play_backward`, 1);
+    oscClient.send(`/medias/${mediaName}/play_forward`, 0);
+};
+
 module.exports.fadeInMaster = fadeInMaster;
 module.exports.fadeOutMaster = fadeOutMaster;
 module.exports.fadeIn = fadeIn;
 module.exports.fadeOut = fadeOut;
 module.exports.show = show;
 module.exports.hide = hide;
-module.exports.resetColorAll = resetColorAll;
 
-module.exports.setRandomOpacity = setRandomOpacity;
-module.exports.setRandomVisibility = setRandomVisibility;
-
+module.exports.resetColor = resetColor;
+module.exports.setRed = setRed;
+module.exports.setGreen = setGreen;
+module.exports.setBlue = setBlue;
 module.exports.setRandomColor = setRandomColor;
 module.exports.setRandomRed = setRandomRed;
 module.exports.setRandomGreen = setRandomGreen;
 module.exports.setRandomBlue = setRandomBlue;
+
+module.exports.setRandomOpacity = setRandomOpacity;
+module.exports.setRandomVisibility = setRandomVisibility;
+
+module.exports.playForward = playForward;
+module.exports.playBackward = playBackward;
--- src/storm/app.js
+++ src/storm/app.js
@@ -1,49 +1,72 @@
 const madmapper = require('../madmapper');
-const patterns = require('./patterns');
 const keypress = require('keypress');
 const tty = require('tty');
 
-let currentPattern = null;
-
 let patternsByKey = {
-    'right': () => {
-        madmapper.fadeInMaster();
+    'right': {
+        patternFunction: () => {
+            madmapper.playForward('Cloodslooped.mov');
+        }
     },
-    'left': () => {
-        madmapper.fadeOutMaster();
+    'left': {
+        patternFunction: () => {
+            madmapper.playBackward('Cloodslooped.mov');
+        }
     },
-    'q': () => {
-        return patterns.pulseFromCenter({
-            speed: 100
-        });
+    '1': {
+        patternFunction: () => {
+            madmapper.fadeOut(['fireStorm']);
+        }
     },
-    'w': () => {
-        // madmapper.hideAll();
-        return patterns.aroundTheWorld({
-            speed: 100
-        });
+    '2': {
+        patternFunction: () => {
+            madmapper.fadeIn(['fireStorm']);
+        }
     },
-    'e': () => {
-        madmapper.hideAll();
-
-        return patterns.randomShow({
-            speed: 100
-        });
+    'q': {
+        patternFunction: () => {
+            madmapper.fadeOut(['redStorm'], 0.7);
+        }
+    },
+    'w': {
+        patternFunction: () => {
+            madmapper.fadeIn(['redStorm'], 0.7);
+        }
+    },
+    'a': {
+        patternFunction: () => {
+            madmapper.fadeOut(['greenStorm'], 0.7);
+        }
+    },
+    's': {
+        patternFunction: () => {
+            madmapper.fadeIn(['greenStorm'], 0.7);
+        }
+    },
+    'z': {
+        patternFunction: () => {
+            madmapper.fadeOut(['blueStorm'], 0.7);
+        }
+    },
+    'x': {
+        patternFunction: () => {
+            madmapper.fadeIn(['blueStorm'], 0.7);
+        }
     }
 };
 
-// make `process.stdin` begin emitting "keypress" events
+// Make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
 
-// listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
-    console.log('got "keypress"', key);
     if (key && key.ctrl && key.name == 'c') {
         process.exit();
     }
-    if (patternsByKey[key.name]) {
-        patterns.clearPattern(currentPattern)
-        currentPattern = patternsByKey[key.name]();
+    let input = ch || key;
+    let inputName = typeof input === 'string' ? input : input.name;
+    console.log(inputName);
+    if (patternsByKey[inputName]) {
+        patternsByKey[inputName].patternFunction();
     }
 });
 
@@ -53,111 +76,3 @@ if (typeof process.stdin.setRawMode == 'function') {
     tty.setRawMode(true);
 }
 process.stdin.resume();
-
-
-function partial(func) {
-    let args = Array.prototype.slice.call(arguments, 1);
-
-    return function() {
-        let allArguments = args.concat(Array.prototype.slice.call(arguments));
-        return func.apply(this, allArguments);
-    };
-}
-
-let patternSchedule = [
-    // {
-    //     patternFunction: partial(patterns.pulseFromCenter, {
-    //         speed: 100
-    //     }),
-    //     duration: 5000
-    // },
-    // {
-    //     patternFunction: partial(patterns.aroundTheWorld, {
-    //         speed: 100
-    //     }),
-    //     duration: 4000
-    // },
-    // {
-    //     patternFunction: partial(patterns.randomShow, {
-    //         speed: 6000
-    //     }),
-    //     duration: 4000
-    // },
-    {
-        patternSetup: () => {
-            madmapper.hideAll();
-        },
-        patternFunction: partial(patterns.randomShow, {
-            speed: 2000
-        }),
-        duration: 20000
-    },
-    {
-        patternFunction: partial(patterns.randomShow, {
-            speed: 1000
-        }),
-        duration: 10000
-    },
-    {
-        patternFunction: partial(patterns.randomShow, {
-            speed: 100
-        }),
-        duration: 5000
-    },
-    {
-        patternFunction: partial(patterns.randomVisibilityAndColor, {
-            speed: 100
-        }),
-        duration: 5000
-    },
-    {
-        patternFunction: partial(patterns.randomOpacity, {
-            speed: 100,
-            surfaceOptions: {
-                sliceLimit: 3
-            }
-        }),
-        duration: 1000
-    },
-    {
-        patternFunction: partial(patterns.randomOpacity, {
-            speed: 100,
-            surfaceOptions: {
-                levelLimit: 2
-            }
-        }),
-        duration: 1000
-    },
-    {
-        patternFunction: partial(patterns.randomOpacity, {
-            speed: 100,
-            surfaceOptions: {
-                levelLimit: 1
-            }
-        }),
-        duration: 1000
-    }
-];
-
-// let currentPattern = null;
-let patternIndex = 0;
-
-// XXX: Loop whole thing
-
-const setPatterns = (pattern) => {
-    if (pattern) {
-        setTimeout(() => {
-            console.log('change pattern');
-
-            patterns.clearPattern(currentPattern, pattern.patternSetup);
-            currentPattern = pattern.patternFunction();
-
-            patternIndex++;
-            // Surprised this doesn't throw an indexing error. Node thing?
-            setPatterns(patternSchedule[patternIndex]);
-        // Look at the duration set on previous pattern to set timeout (confusing but makes the scheduler easier to understand)
-        }, patternIndex === 0 ? 0 : patternSchedule[patternIndex - 1].duration);
-    }
-};
-
-// setPatterns(patternSchedule[patternIndex]);
