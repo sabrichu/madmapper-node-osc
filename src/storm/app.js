9a4ca53a046b8e41c4b2916c35e8a16c9e2f0c64
sabrichu <sabrichu@gmail.com>
2017-12-09 17:21:50 -0800

    Control candy mountain with keys

--- src/storm/app.js
+++ src/storm/app.js
@@ -4,106 +4,185 @@ const tty = require('tty');
 
 let sourceY = -1.9;
 let sourceYLimit = 1.9;
-let yUnit = 0.02;
+let yUnit = 0.25;
+
+let mode = 'inkSink';
+
+let surfaces = {
+    'candyMountain': ['candyMountain', 'candySky', 'candyRain1', 'candyRain2'],
+    'fireShrum': ['redStorm', 'blueStorm', 'greenStorm', 'fireStorm'],
+    'inkSink': ['redSink', 'yellowSink', 'tealSink', 'inkSink']
+};
 
 let patternsByKey = {
+    '7': {
+        patternFunction: () => {
+            console.log('--- INK SINK ---')
+            mode = 'inkSink';
+            madmapper.hide(surfaces.candyMountain);
+            madmapper.hide(surfaces.fireShrum);
+            madmapper.show(surfaces.inkSink);
+        }
+    },
     '8': {
         patternFunction: () => {
             console.log('--- FIRE SHRUM ---')
-            madmapper.fadeOut(['candyMountain', 'candySky']);
-            madmapper.fadeIn(['redStorm', 'blueStorm', 'greenStorm', 'fireStorm']);
+            mode = 'fireShrum';
+            madmapper.hide(surfaces.candyMountain);
+            madmapper.hide(surfaces.inkSink);
+            madmapper.show(surfaces.fireShrum);
         }
     },
     '9': {
         patternFunction: () => {
             console.log('--- CANDY MOUNTAIN ---')
-            madmapper.fadeOut(['redStorm', 'blueStorm', 'greenStorm', 'fireStorm']);
-            madmapper.fadeIn(['candyMountain', 'candySky']);
+            mode = 'candyMountain';
+            madmapper.hide(surfaces.fireShrum);
+            madmapper.hide(surfaces.inkSink);
+            madmapper.show(surfaces.candyMountain);
         }
     },
     '0': {
         patternFunction: () => {
             console.log('--- ALL OFF ---')
-            madmapper.fadeOut(['redStorm', 'blueStorm', 'greenStorm', 'fireStorm', 'candyMountain', 'candySky']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeOut(surfaces.fireShrum);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeOut(surfaces.inkSink);
+            } else if (mode === 'candyMountain') {
+                madmapper.fadeOut(surfaces.candyMountain);
+            }
         }
     },
     'up': {
         patternFunction: () => {
-            console.log('Storm triangle up');
+            console.log('Bleed up');
             sourceY += yUnit;
             if (sourceY >= sourceYLimit) {
                 sourceY = sourceYLimit - yUnit;
             }
             madmapper.moveSourceY('fireStorm', sourceY);
+            madmapper.moveSourceY('inkSink', sourceY);
+            madmapper.moveSourceY('redSink', sourceY);
+            madmapper.moveSourceY('yellowSink', sourceY);
+            madmapper.moveSourceY('tealSink', sourceY);
         }
     },
     'down': {
         patternFunction: () => {
-            console.log('Storm triangle down');
+            console.log('Bleed down');
             sourceY -= yUnit;
             if (sourceY <= -sourceYLimit) {
                 sourceY = -sourceYLimit + yUnit;
             }
             madmapper.moveSourceY('fireStorm', sourceY);
+            madmapper.moveSourceY('inkSink', sourceY);
+            madmapper.moveSourceY('redSink', sourceY);
+            madmapper.moveSourceY('yellowSink', sourceY);
+            madmapper.moveSourceY('tealSink', sourceY);
         }
     },
     'right': {
         patternFunction: () => {
-            console.log('Storm forward');
+            console.log('Movie forward');
             // Cloods backward is forward
             madmapper.playBackward('Cloodslooped.mov');
+            madmapper.playForward('inklooped-prism-cropped.mov');
         }
     },
     'left': {
         patternFunction: () => {
-            console.log('Storm backward');
+            console.log('Movie backward');
             madmapper.playForward('Cloodslooped.mov');
+            madmapper.playBackward('inklooped-prism-cropped.mov');
         }
     },
     '1': {
         patternFunction: () => {
-            madmapper.fadeOut(['fireStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeOut(['fireStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeOut(['inkSink']);
+            }
         }
     },
     '2': {
         patternFunction: () => {
-            madmapper.fadeIn(['fireStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeIn(['fireStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeIn(['inkSink']);
+            }
         }
     },
     'q': {
         patternFunction: () => {
             console.log('red down')
-            madmapper.fadeOut(['redStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeOut(['redStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeOut(['redSink']);
+            } else if (mode === 'candyMountain') {
+                madmapper.fadeOut(['candyMountain']);
+            }
         }
     },
     'w': {
         patternFunction: () => {
             console.log('red up')
-            madmapper.fadeIn(['redStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeIn(['redStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeIn(['redSink']);
+            } else if (mode === 'candyMountain') {
+                madmapper.fadeIn(['candyMountain']);
+            }
         }
     },
     'a': {
         patternFunction: () => {
             console.log('green down')
-            madmapper.fadeOut(['greenStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeOut(['greenStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeOut(['yellowSink']);
+            } else if (mode === 'candyMountain') {
+                madmapper.fadeOut(['candyRain1']);
+                madmapper.fadeOut(['candyRain2']);
+            }
         }
     },
     's': {
         patternFunction: () => {
             console.log('green up')
-            madmapper.fadeIn(['greenStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeIn(['greenStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeIn(['yellowSink']);
+            } else if (mode === 'candyMountain') {
+                madmapper.fadeIn(['candyRain1']);
+                madmapper.fadeIn(['candyRain2']);
+            }
         }
     },
     'z': {
         patternFunction: () => {
             console.log('blue down')
-            madmapper.fadeOut(['blueStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeOut(['blueStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeOut(['tealSink']);
+            }
         }
     },
     'x': {
         patternFunction: () => {
             console.log('blue up')
-            madmapper.fadeIn(['blueStorm']);
+            if (mode === 'fireShrum') {
+                madmapper.fadeIn(['blueStorm']);
+            } else if (mode === 'inkSink') {
+                madmapper.fadeIn(['tealSink']);
+            }
         }
     }
 };
